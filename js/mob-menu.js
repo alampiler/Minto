ba01581ff58412b9cb8ef5f19ef3708b4cbbffab
MoRdreD (Vadim Vozniuk) <47083343+alampiler@users.noreply.github.com>
2019-02-08 14:44:51 +0200

    Add full project

---
+++ js/mob-menu.js
@@ -0,0 +1,12 @@
+  var mobBtn = document.getElementById("navbar-mob-btns");
+
+  function mobMenu() {
+  	if (mobBtn.style.display === "block") {
+  		mobBtn.style.display = "none";
+  	} else {
+  		mobBtn.style.display = "block";
+  	}
+  }
+
+
+
---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('.slider-container').slick({
+    infinite: true,
+    dots: false,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true
+  });
+});
